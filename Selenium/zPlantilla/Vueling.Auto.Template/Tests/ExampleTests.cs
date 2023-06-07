303b6613893d4fa8ce11e676d83f9755d7c7ccbf
Joel Olivera <joel.olivera@col.vueling.com>
2023-06-07 22:38:29 +0200

    The custom template for selenium tests

---
+++ Selenium/zPlantilla/Vueling.Auto.Template/Tests/ExampleTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Plantilla.Selenium.Common;
+using Plantilla.Seleniums;
+
+namespace Plantilla.Selenium.Tests
+{
+    // Assert.AreEqual(expected, actual): Verifies that two values are equal.
+    // Assert.IsTrue(condition): Verifies that a condition is true.
+    // Assert.IsFalse(condition): Verifies that a condition is false.
+    // Assert.IsNull(obj): Verifies that an object is null.
+    // Assert.IsNotNull(obj): Verifies that an object is not null.
+    [TestFixture]
+    class ExampleTests : TestSetCleanBase
+    {
+        [TestCase, Order(1)]
+        // Flow to Register
+        public void registerAnUser()
+        {
+            registerPage = new ExamplePage(setUpWebDriver);
+            registerPage.registerNewClient("Pedro", "Sanchez", "adaadw", Helpers.GetRandomPhoneNumber(), "adwadc");
+        }
+    }
+}
---
+++ Selenium/zPlantilla/Vueling.Auto.Template/WebPages/ExamplePage.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Plantilla.Selenium.Common;
+using Plantilla.Selenium.SetUp;
+using Plantilla.Selenium.WebPages.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Plantilla.Selenium
+{
+    public class ExamplePage : CommonPage
+    {
+        public ExamplePage(ISetUpWebDriver setUpWebDriver) : base(setUpWebDriver)
+        {
+        }
+
+        protected override IWebElement ApartadosBusqueda => throw new NotImplementedException();
+
+        private IWebElement fieldFName => WebDriver.FindElementById("input-firstname");
+        private IWebElement fieldLName => WebDriver.FindElementById("input-lastname");
+        private IWebElement fieldMail => WebDriver.FindElementById("input-email");
+        private IWebElement fieldTel => WebDriver.FindElementById("input-telephone");
+        private IWebElement fieldPasswd => WebDriver.FindElementById("input-password");
+        private IWebElement fieldREPasswd => WebDriver.FindElementById("input-confirm");
+        private IWebElement radioYes2Subs => WebDriver.FindElementByXPath("//input[@name='newsletter' and @value=1]");
+        private IWebElement radioNo2Subs => WebDriver.FindElementByXPath("//input[@name='newsletter' and @value=0]");
+        private IWebElement checkPrivacyP => WebDriver.FindElementByXPath("//input[@name='agree']");
+        private IWebElement btnContinue => WebDriver.FindElementByXPath("//input[@value='Continue']");
+        private By btnHome => By.XPath("//div[@id='logo']//a");
+        private IWebElement _btnHome => WebDriver.FindElement(btnHome);
+
+
+        public ExamplePage registerNewClient(String fname, String lname, String email, int tel, String passwd)
+        {
+            fieldFName.SendKeys(fname); 
+            fieldLName.SendKeys(lname);
+            fieldMail.SendKeys(email);
+            fieldTel.SendKeys(tel.ToString());
+            fieldPasswd.SendKeys(passwd);
+            fieldREPasswd.SendKeys(passwd);
+            radioNo2Subs.Click();
+            checkPrivacyP.Click();
+            btnContinue.Click();
+            new WebDriverWait(WebDriver, TimeSpan.FromSeconds(WaitTimeout)).Until(CustomExpectedConditions.ElementIsVisible(btnHome));
+            Assert.AreEqual("Congratulations! Your new account has been successfully created!", WebDriver.FindElementByXPath("//div[@id='content']/p[1]").Text);
+            _btnHome.Click();
+            return this;
+        }
+
+    }
+}
