481a661898b6c6c36d241dbfa56191a5f0121b68
Joel Olivera <joel.olivera@col.vueling.com>
2023-06-16 08:48:32 +0200

    New one way flight for vueling page with Cypress

--- Cypress/Vueling/cypress/e2e/FlightTests.cy.js
+++ Cypress/Vueling/cypress/e2e/FlightTests.cy.js
@@ -1,21 +1,37 @@
 import { HomePage } from "../webpages/HomePage";
 
-describe("UserTests", () => {
+describe("FlightTests", () => {
 	const homePage = new HomePage();
+	const dayjs = require("dayjs");
+	let dataFlight = {};
+
+	before(() => {
+		cy.fixture("dataOWFlight").then(($data) => {
+			dataFlight = $data;
+		});
+	});
 
 	beforeEach(() => {
 		cy.visit("");
 	});
 
-	it("Verify to search One Way flight", () => {
-		/*
-		BCN-ATH (Atenas)
-		Solo ida
-		2 Adultos, 1 Bebe
-		Primer dia disponible del mes de agosto
-		Click en Buscar Vuelo
-		*/
+	it("OWTC0. Verify to search One Way flight", () => {
 		homePage.acceptCookies();
-		homePage.searchOWFlight("BCN", "ATH");
+		homePage.searchOWFlight(
+			dataFlight.originCode,
+			dataFlight.destCode,
+			dataFlight.monthNum,
+			dataFlight.adtQTY,
+			dataFlight.chdQTY,
+			dataFlight.infQTY
+		);
+	});
+
+	afterEach(() => {
+		cy.screenshot(
+			Cypress.mocha.getRunner().suite.ctx.currentTest.title +
+				dayjs().format("_YYYY-MM-DD_HH.mm.ss")
+		);
+		cy.addContext("Screenshot taken. You can see it in ./cypress/screenshots");
 	});
 });
--- Cypress/Vueling/cypress/webpages/HomePage.js
+++ Cypress/Vueling/cypress/webpages/HomePage.js
@@ -12,6 +12,20 @@ export class HomePage {
 		cy.getId("AvailabilitySearchInputSearchView_TextBoxMarketDestination1");
 	containerFlightOptions = () => cy.getId("stationsList");
 	btnFlightOption = (code) => cy.get("[data-id-code='" + code + "']");
+	textCalendarMonth = () => cy.get("[data-month]");
+	btnCalendarNextMonth = () => cy.get(".ui-datepicker-next");
+	btnCalendarDay = () => cy.get("[data-handler='selectDay']");
+	btnAdultDropDown = () => cy.getId("DropDownListPassengerType_ADT_PLUS");
+	optionAdult = () => cy.getId("adtSelectorDropdown");
+	btnChildrenDropDown = () => cy.get(".column_3");
+	optionChildren = () =>
+		cy.getId("AvailabilitySearchInputSearchView_DropDownListPassengerType_CHD");
+	btnInfantDropDown = () => cy.get(".column_4.buscador_pasajeros_childs");
+	optionInfant = () =>
+		cy.getId(
+			"AvailabilitySearchInputSearchView_DropDownListPassengerType_INFANT"
+		);
+	btnSearchFlights = () => cy.getId("divButtonBuscadorNormal");
 
 	// * Methods
 	acceptCookies() {
@@ -20,8 +34,18 @@ export class HomePage {
 		});
 	}
 
-	searchOWFlight(originCode, destCode) {
-		this.btnOneWay().click();
+	changeMonth(monthNum) {
+		this.textCalendarMonth()
+			.invoke("attr", "data-month")
+			.then(($attr) => {
+				if ($attr != monthNum) {
+					this.btnCalendarNextMonth().click();
+					this.changeMonth(monthNum);
+				}
+			});
+	}
+
+	selectOrigDestFlight(originCode, destCode) {
 		this.btnFlightOrigin().type(originCode, { force: true });
 		this.containerFlightOptions().should("exist");
 		this.btnFlightOption(originCode).click();
@@ -29,5 +53,30 @@ export class HomePage {
 		this.btnFlightOption(destCode).should("be.visible");
 		this.containerFlightOptions().should("exist");
 		this.btnFlightOption(destCode).click();
+	}
+
+	selectPassengers(adtQTY, chdQTY, infQTY) {
+		this.btnAdultDropDown().should("be.visible");
+		this.btnAdultDropDown().click();
+		this.optionAdult().select(adtQTY);
+		this.optionAdult().should("have.value", adtQTY);
+		this.btnChildrenDropDown().click();
+		this.optionChildren().select(chdQTY);
+		this.optionChildren().should("have.value", chdQTY);
+		this.btnInfantDropDown().click();
+		this.optionInfant().select(infQTY);
+		this.optionInfant().should("have.value", infQTY);
+	}
+
+	searchOWFlight(originCode, destCode, monthNum, adtQTY, chdQTY, infQTY) {
+		this.btnOneWay().click();
+		this.selectOrigDestFlight(originCode, destCode);
+		this.textCalendarMonth().should("be.visible");
+		// ! Need to subs 1 due that months go from 0 to 11, so it's 1 less than the normal
+		monthNum--;
+		this.changeMonth(monthNum);
+		this.btnCalendarDay().first().click();
+		this.selectPassengers(adtQTY, chdQTY, infQTY);
+		this.btnSearchFlights().click();
 	}
 }
